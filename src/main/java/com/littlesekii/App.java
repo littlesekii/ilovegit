b775fbf90d548f63957ba42a623569aaaf00db76
littlesekii <contact.sekii@gmail.com>
2025-07-18 13:09:04 -0300

    removed dev branch identifier

--- src/main/java/com/littlesekii/App.java
+++ src/main/java/com/littlesekii/App.java
@@ -12,7 +12,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("~You are in the Dev branch.");
         System.out.println("===========================");
 
         Pokemon pokemon = new Pikachu();
